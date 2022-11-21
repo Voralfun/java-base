7138d95b5020b6381689b20f49bf1a7e3fdb5bcd
Voralfun <rmguy24@gmail.com>
2022-11-21 14:25:48 +0300

    add cod for second task

---
+++ src/itmo/lesson2/WithParameters.java
@@ -0,0 +1,12 @@
+package itmo.lesson2;
+
+public class WithParameters {
+    int x;
+    int y;
+    int z;
+    public WithParameters(int x,int y,int z) {
+        this.x = x;
+        this.y = y;
+       this.z = z;
+    }
+}
