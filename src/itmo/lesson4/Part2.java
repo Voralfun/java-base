6db836755b1b0062b1c59c00c5f49fbbb3f6d506
Voralfun <rmguy24@gmail.com>
2022-11-29 17:09:26 +0300

    add class SomeMethods for code for 4 lesson

--- src/itmo/lesson4/Part2.java
+++ src/itmo/lesson4/Part2.java
@@ -4,20 +4,20 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Part2 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         //Task 1
         boolean isSorted = true;
-        int [] arr = new int[]{1,2,3,4,5};
-        for (int i = 0; i < arr.length-1; i++) {
-                if (arr[i] > arr[i+1]) {
-                    isSorted = false;
-                    break;
-                }
+        int[] arr = new int[]{1, 2, 3, 4, 5};
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                isSorted = false;
+                break;
+            }
         }
         System.out.println(Arrays.toString(arr));
-        if (isSorted == true){
+        if (isSorted == true) {
             System.out.println("Okay");
-        }else{
+        } else {
             System.out.println("Please, try again");
         }
 
@@ -26,21 +26,21 @@ public class Part2 {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter array length:");
         int arrSize = scanner.nextInt();
-        int [] arr2 = new int[arrSize];
+        int[] arr2 = new int[arrSize];
         for (int i = 0; i < arrSize; i++) {
             System.out.println("Enter number:");
-            arr2[i]=scanner.nextInt();
+            arr2[i] = scanner.nextInt();
         }
         System.out.println(Arrays.toString(arr2));
 
         //Task 3
-        int [] arr3 = new int[]{3,4,5,9};
-        System.out.println("Not sorted array:"+Arrays.toString(arr3));
-        int [] arrSwap = arraySwap(arr3);
-        System.out.println("Sorted array:"+Arrays.toString(arrSwap));
+        int[] arr3 = new int[]{3, 4, 5, 9};
+        System.out.println("Not sorted array:" + Arrays.toString(arr3));
+        int[] arrSwap = SomeMethods.arraySwap(arr3);
+        System.out.println("Sorted array:" + Arrays.toString(arrSwap));
 
         //Task 4
-        int [] arr4 = new int[]{4,3,2,3,4,7,9,1,1};
+        int[] arr4 = new int[]{4, 3, 2, 3, 4, 7, 9, 1, 1};
         int first = 0;
         for (int i = 0; i < arr4.length; i++) {
             boolean isUnique = true;
@@ -50,18 +50,14 @@ public class Part2 {
                     break;
                 }
             }
-            if(isUnique){
-                if(first == 0){
-                    System.out.println("Unique numbers:"+arr4[i]);
-                    first++;}
+            if (isUnique) {
+                if (first == 0) {
+                    System.out.println("Unique numbers:" + arr4[i]);
+                    first++;
                 }
+            }
         }
     }
-
-    public static int[] arraySwap(int [] array){
-        int x = array[0];
-        array[0] = array[array.length-1];
-        array[array.length-1]= x;
-        return array;
-    }
 }
+
+
